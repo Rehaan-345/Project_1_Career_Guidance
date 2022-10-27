e00af87c02c533a0f470720a71658f5cd631b378
Rehaan-345 <116780374+Rehaan-345@users.noreply.github.com>
2022-10-27 11:38:53 +0530

    edit conn string

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB).then(() => {
+mongoose.connect("https://github.com/Rehaan-345/Project_1_Career_Guidance/blob/main/src/db").then(() => {
     console.log("connection successful");
 }).catch((e) => {
     console.log('no connection'+e);
